ff0f7725e208df514ff8a067c805af78b9bb28d5
Erin Rutkowski-jones <100587683@oncampus.local>
2017-07-31 21:18:50 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect= require('expect');
 const request= require('supertest');
 
+const {ObjectID}= require('mongodb');
 // write ../ to go back a directory
 const {app}= require('./../server');
 const {Todo}= require('./../models/todo');
 
 const todos= [{
-  text: 'First test todo',
-  text: 'Second test todo'
+  _id: new ObjectID(),
+  text: 'First test todo'},
+  { _id: new ObjectID(),
+    text: 'Second test todo'
 }];
 
 //test the todos collection to see if there's any todos in there before we start the code
@@ -60,7 +63,7 @@ describe('POST /todos', () => {
       //if there are no errors, fetch all todos of the collection
       Todo.find().then((todos) => {
         //let's make sure that there are no todos
-        expect(todos.length).toBe(1);
+        expect(todos.length).toBe(2);
         //exeunt
         done();
       }).catch((e) => done(e));
@@ -75,8 +78,38 @@ describe('GET /todos', () => {
       .get('/todos')
       .expect(200)
       .expect((res) => {
-        expect(res.body.todos.length).toBe(1);
+        expect(res.body.todos.length).toBe(2);
       })
       .end(done);
   });
 });
+
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  var hexId= new ObjectID().toHexString();
+
+  it('should return 404 if the todo not found', (done) => {
+    //make sure you get a 404 back
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+    });
+  });
