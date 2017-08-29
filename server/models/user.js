2acc2addce3d00fa6cb11d31e1eeb1ea93026f5b
Erin Rutkowski-jones <100587683@oncampus.local>
2017-08-29 17:13:33 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -78,6 +78,31 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials= function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    //if user doesn't exist
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      //use bcrypt.compare to compare password and user password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        }
+        else{
+          reject();
+        }
+      })
+      //if no match-reject
+      //if match-resolve
+    });
+  });
+};
+
 
 UserSchema.pre('save', function (next) {
   var user= this;
@@ -91,7 +116,7 @@ UserSchema.pre('save', function (next) {
         user.password=hash;
         next();
       });
-    });   
+    });
   } else {
     next();
   }
