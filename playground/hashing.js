03d91d576241e043efa3c28e6b14db78e3efa8db
Erin Rutkowski-jones <100587683@oncampus.local>
2017-08-28 20:52:59 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,52 +1,73 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt= require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-//sign creates the salted hash- takes in the object and the 'somesecretstring'
-var token = jwt.sign(data, '123abc');
-console.log(token);
+//let's hash it using genSalt and the hashing process
 
-//takes the token and makes sure the data wasn't manipulated
-//returns an error if the token is incorrect
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+//generate the salt
+//bcrypt puts the salt into the password
+//the number in the brackets here is the number of 'rounds', the bigger the number- the longer it takes to process
+// bcrypt.genSalt(10, (err, salt) => {
+//   //let's hash it
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-//the following is what is accomplished within the jsonwebtoken library
 
-// //let's hash something!
-// var message = "I am the ultimate user";
-// var hash = SHA256(message).toString();
-//
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
+var hashedPassword = '$2a$10$xGOimqGCaPGUEXVVdXiQtuA5CwYRoAUhRly7klfQ.q2iRtJvr8HBe';
 
-// var data= {
-//   id: 4
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
 // };
 //
+// //sign creates the salted hash- takes in the object and the 'somesecretstring'
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 //
-// var token = {
-//   data,
-//   hash: SHA256(JSON.stringify(data) + 'somesecretstring').toString()
-// }
+// //takes the token and makes sure the data wasn't manipulated
+// //returns an error if the token is incorrect
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 //
-// //hash the token data
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecretstring').toString();
+// //the following is what is accomplished within the jsonwebtoken library
 //
-// // // this could be a troublemaker trying to change the value of the data- this is what salting helps fight against
-// // token.data.id=5;
-// // token.hash= SHA256(JSON.stringify(token.data)).toString();
+// // //let's hash something!
+// // var message = "I am the ultimate user";
+// // var hash = SHA256(message).toString();
+// //
+// // console.log(`Message: ${message}`);
+// // console.log(`Hash: ${hash}`);
 //
-// //check if the hashed token data is the same as the 'hash' section of token
-// //they should be the same
-// if (resultHash === token.hash) {
-//   console.log("data was not changed");
-// }
-// else {
-//   console.log('data was changed. DO NOT TRUST')
-// }
-// //salting the hash, adds something on to the hash to make it unique everytime
-// //the salt added here was 'somesecretstring'
+// // var data= {
+// //   id: 4
+// // };
+// //
+// //
+// // var token = {
+// //   data,
+// //   hash: SHA256(JSON.stringify(data) + 'somesecretstring').toString()
+// // }
+// //
+// // //hash the token data
+// // var resultHash = SHA256(JSON.stringify(token.data) + 'somesecretstring').toString();
+// //
+// // // // this could be a troublemaker trying to change the value of the data- this is what salting helps fight against
+// // // token.data.id=5;
+// // // token.hash= SHA256(JSON.stringify(token.data)).toString();
+// //
+// // //check if the hashed token data is the same as the 'hash' section of token
+// // //they should be the same
+// // if (resultHash === token.hash) {
+// //   console.log("data was not changed");
+// // }
+// // else {
+// //   console.log('data was changed. DO NOT TRUST')
+// // }
+// // //salting the hash, adds something on to the hash to make it unique everytime
+// // //the salt added here was 'somesecretstring'
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose= require('mongoose');
 const validator= require('validator');
 const jwt= require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt= require('bcryptjs');
 
 //store the schema (the properties) of the user
 var UserSchema = new mongoose.Schema( {
@@ -76,6 +77,25 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+
+UserSchema.pre('save', function (next) {
+  var user= this;
+
+  //check if the password was modified
+  if (user.isModified('password')) {
+    //call gensalt and hash
+    bcrypt.genSalt(10, (err, salt) => {
+      //let's hash it
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password=hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User= mongoose.model('User', UserSchema);
 
